2b3a8c59b13d056bf4719d52960b1f3eed8e216b
imhonggeun <dkfl9870@gmail.com>
2025-07-31 15:02:38 +0900

    정리

--- 2025731/main.py
+++ 2025731/main.py
@@ -5,21 +5,25 @@ data = pd.read_csv('./data1_20220731.csv')
 
 # Q2 위의 인구수 표에서 '세대수'가 가장 높은 동명은 무엇인가요?
 d1 = data.sort_values(by=['세대수'],ascending=[0])
-print(d1)
-#print(d1.iloc[0].loc["동명"])
-
-# Q3 위의 인구수 표에서 '남(65세 이상)' 인구수가 높은 동명은 
-# 무엇인가요?
-#d1 = data.sort_values(by=['남(65세이상)'],ascending=[0])
 #print(d1)
 #print(d1.iloc[0])
 #print(d1.iloc[0].loc["동명"])
 
+
+
+# Q3 위의 인구수 표에서 '남(65세 이상)' 인구수가 높은 동명은 
+# 무엇인가요?
+d2 = data.sort_values(by=['남(65세이상)'],ascending=[0])
+#print(d2)
+#print(d2.iloc[0])
+#print(d2.iloc[0].loc["동명"])
+
+
 # Q4 위의 인구수 표에서 '세대수'의 평균 이상 인구수의 동명은 무엇인가요? 
-#평균값 = data['세대수'].mean().astype(int)
-#data = data[data['세대수'] > 평균값]
-#print(data)
-#print(data["동명"])
+평균값 = data['세대수'].mean().astype(int)
+data = data[data['세대수'] > 평균값]
+print(data)
+print(data["동명"])
 
 
 #동근
